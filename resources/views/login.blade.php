c01a321306b5954e85c519de9fc7931558c92b80
Gmagxcx <magooo.xyz@gmail.com>
2024-11-16 13:12:04 +0800

    Login and Signup functionalities added (NO COOKIES YET)
    -Refresh migrations before testing
    
    -CSS files moved to root folder under /public
    
    -minor changes to home page
    
    functional validations with error messages per input field

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -38,16 +38,6 @@
             </div>
         </form>
 
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
         
     </div>
 </body>
