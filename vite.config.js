0445f8d3fb6546e5304b861db11fbb12bc4df979
Gmagxcx <magooo.xyz@gmail.com>
2024-12-17 23:31:12 +0800

    vite config

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,8 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'build', // Output to "build" directory
+        emptyOutDir: true, // Clears previous builds
+    },
 });
